ed81acf167ac6463aba5851d450715d01dc16751
ivanC-M <ivan@ayhungry.com>
2026-06-01 18:44:33 -0400

    Refactor ReactDOM usage in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-const rootElement = document.getElementById("root")!;
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
+const rootElement = document.getElementById("root");
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  rootElement
 );
